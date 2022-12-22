d51fac5795f335380632c9178d99efe65f73b500
unknown <schuldjack@gmail.com>
2022-12-22 12:43:27 -0600

    Uploading rough draft with whole game complete. Next steps is to figure out how to have the bot auto select

--- gui.py
+++ gui.py
@@ -12,16 +12,16 @@ class Gui:
 
     # Goes through gui methods to run the game
     def run_game(self):
-        self.start_game()
+        self.welcome()
         self.instructions()
         self.create_player()
         self.vs_human_or_ai()
-        for player in self.players:
-            print(player.name)
+        self.play()
+        self.game_over()
 
 
-    # Displays welcome
-    def start_game(self):
+    # Displays welcome message
+    def welcome(self):
         self.new_section()
         print('''                       Welcome to RPSLS!
                 
@@ -47,8 +47,8 @@ Rules:
         1. against an ai
         2. against a friend
     - Take turns entering 'Rock', 'Paper', 'Scissors', 'Lizard', or 'Spock'.
-    - If your selection wins against the opponents, you earn a point.
-    - Play to the best of 3 (ie. whoever gets 2 wins first).
+    - If your selection wins against the opponent's, you earn a point.
+    - Play to the best of 3 (ie. first to win 2 rounds wins the game).
     
 Determine Winner:
     - Rock > Scissors
@@ -61,7 +61,6 @@ Determine Winner:
     - Lizard > Paper
     - Spock > Scissors
     - Spock > Rock
-
         ''')
         input('[ Press Enter to Continue ]')
     
@@ -90,16 +89,63 @@ Determine Winner:
         self.players += [Human(player_name)]
 
 
+    # Select human or generate ai
     def vs_human_or_ai(self):
         self.new_section()
         if self.yes_or_no('Create another Human? ') == 'Y':
             self.create_player()
         else:
-            gen_bot_name = 'bot#' + str(randint(0, 1001)) 
-            self.players += [Ai(gen_bot_name)]
-        
+            self.gen_bot()
 
-
-    def turn(self):
-        self.new_section()
     
+    # Generates bot name and adds to self.players
+    def gen_bot(self):
+        gen_bot_name = 'bot#' + str(randint(0, 1001)) 
+        self.players += [Ai(gen_bot_name)]
+    
+
+    # Runs main part of game until there is a winner
+    def play(self):
+        self.new_section('start')
+        round_counter = 1
+        while self.players[0].wins < 2 and self.players[1].wins < 2:
+            self.new_section('Round ' + str(round_counter))
+            self.get_selections()
+            self.compare_selections()
+            round_counter += 1
+        
+    
+    def get_selections(self):
+        self.players[0].set_selection()
+        self.players[1].set_selection()
+        print(f'{self.players[0].selection} vs {self.players[1].selection}')
+        time.sleep(1)
+    
+
+    # Compares selections to give a player a point or detect a draw
+    def compare_selections(self):
+        if self.players[0].selection == self.players[1].selection:
+            print('Draw!\nGo again\n')
+        elif self.players[1].selection in self.players[0].gestures[self.players[0].selection]:
+            print(f'{self.players[0].name} wins this round!')
+            self.players[0].wins += 1
+        else:
+            print(f'{self.players[1].name} wins this round!')
+            self.players[1].wins += 1
+        self.scoreboard()
+
+
+    def scoreboard(self):
+        self.new_section('scoreboard')
+        print('Points:')
+        for player in self.players:
+            print(f'{player.name} - {player.wins}')
+        input('\n[ Press Enter to Continue ]')
+
+    
+    def game_over(self):
+        self.new_section('winner')
+        if self.players[0].wins > 1:
+            print(f'Congrats {self.players[0].name}! You won RPSLS best of 3!\n')
+        else:
+            print(f'Congrats {self.players[1].name}! You won RPSLS best of 3!\n')
--- player.py
+++ player.py
@@ -2,4 +2,22 @@ class Player:
 
     def __init__(self, name):
         self.name = name
-        self.gestures = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+        self.wins = 0
+        # Made into dictionary, which has gesture as the key and the values are the gestures the key will beat
+        self.gestures = {'Rock': ['Scissors', 'Lizard'], 'Paper': ['Spock', 'Rock'], 'Scissors': ['Paper', 'Lizard'], 'Lizard': ['Paper', 'Spock'], 'Spock': ['Rock', 'Scissors']}
+        self.selection = ''
+    
+
+    def set_selection(self):
+        selection = ''
+        while selection not in self.gestures:
+            print(f'\n{self.name}\'s turn!')
+            for gesture in self.gestures:
+                print(f'- ' + gesture)
+            selection = input(f'\n{self.name} - Type out the option from above that you want to select: ').title()
+        self.selection = selection
+        input('\n[ Press Enter to Continue ]')
+        print('\n')
+
+
+    
